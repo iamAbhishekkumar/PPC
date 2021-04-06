33c6f06fe8d212d986c228d65c99f108966c0927
gymy <abhisonuk123@gmail.com>
2021-04-06 23:21:01 +0530

    added basic template generator

--- extension.js
+++ extension.js
@@ -1,24 +1,32 @@
 const vscode = require('vscode');
+const fs = require('fs');
+const template = require('./scripts/templates');
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+
+
 function activate(context) {
 	console.log("Active")
 	context.subscriptions.push(vscode.commands.registerCommand('ppc.createPythonProject', async () => {
-		let currentDirectoryPath = vscode.workspace.workspaceFolders[0].uri.path;
-		if (currentDirectoryPath != null) {
-			vscode.window.showInformationMessage(currentDirectoryPath);
+		let currentDirectory = vscode.workspace.workspaceFolders[0].uri.path;
+		if (currentDirectory != null) {
+			vscode.window.showInformationMessage(currentDirectory);
 			try {
-				const options = ['basic','advance','flask-App'];
+				const options = ['Basic', 'Installable Single Package', 'flask-App'];
 				console.log(options);
 				const quickPick = vscode.window.createQuickPick();
 				quickPick.items = options.map(label => ({ label }));
-				quickPick.onDidChangeSelection(([{ label }]) => {
-					if(label == 'basic'){
+				quickPick.onDidChangeSelection(async ([{ label }]) => {
+					if (label == 'basic') {
 						console.log("User chooses basic");
-						
+						const folderName = await vscode.window.showInputBox();
+						createBasicTemplate(currentDirectory + "/" + folderName);
 					}
+					quickPick.hide();
 				});
 				quickPick.show();
 				quickPick.onDidHide(() => quickPick.dispose());
@@ -26,6 +34,7 @@ function activate(context) {
 			}
 			catch (err) {
 				console.log(err)
+				vscode.window.showInformationMessage("Unexpected Error Occured");
 			};
 
 		} else
@@ -39,7 +48,66 @@ exports.activate = activate;
 
 function deactivate() { }
 
+
+// folder_generated/
+// │
+// ├── .gitignore
+// ├── main.py
+// ├── LICENSE
+// ├── README.md
+// ├── requirements.txt
+// ├── setup.py
+// └── tests.py
+
+function createBasicTemplate(folderPath) {
+	console.log(folderPath);
+	if (!fs.existsSync(folderPath)) {
+		fs.mkdirSync(folderPath);
+		fs.appendFile(folderPath + '/.gitignore', template.gitIgnoreContent, function (err) {
+			if (err) throw err;
+			console.log('gitignore saved !');
+		});
+
+		fs.appendFile(folderPath + '/main.py', "", function (err) {
+			if (err) throw err;
+			console.log('main.py saved!');
+		});
+
+		
+		// TODO :  Give option for creating LICENSE TYPE
+		fs.appendFile(folderPath + '/LICENSE', "", function (err) {
+			if (err) throw err;
+			console.log('LICENSE saved!');
+		});
+
+		// TODO: put this in templates.js and capitalize first letter
+		const readmeContent = `# ${folderPath.split("/")[folderPath.split("/").length - 1]}`;
+		fs.appendFile(folderPath + '/README.md', readmeContent, function (err) {
+			if (err) throw err;
+			console.log('Readme saved!');
+		});
+
+		fs.appendFile(folderPath + '/setup.py', "", function (err) {
+			if (err) throw err;
+			console.log('setup.py saved!');
+		});
+
+		fs.appendFile(folderPath + '/tests.py', "", function (err) {
+			if (err) throw err;
+			console.log('tests.py saved!');
+		});
+
+	}
+}
+
+
+
+
+
+
+
 module.exports = {
 	activate,
 	deactivate
 }
+
